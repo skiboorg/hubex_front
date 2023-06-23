b0e839e53fa00395d2b20f33c9bf27828aa5f9cb
xxx <xxx@xxx.xxx>
2023-06-23 15:54:31 +0300

    2

--- src/layouts/Driver.vue
+++ src/layouts/Driver.vue
@@ -2,12 +2,13 @@
   <q-layout view="lHh Lpr lFf">
     <q-header>
       <q-toolbar>
+        <q-btn v-if="$route.path !== '/worker'" @click="$router.back()" icon="chevron_left" flat   />
         <q-toolbar-title>
           <span>{{auth.user?.fio}}</span><br>
           <span class="text-caption">{{auth.user?.role.name}}</span>
         </q-toolbar-title>
         <q-space/>
-        <q-btn icon="menu"/>
+        <q-btn icon="menu" dense flat round/>
       </q-toolbar>
 
     </q-header>
